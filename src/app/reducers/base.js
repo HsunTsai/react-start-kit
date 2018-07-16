c515f15aec6237c0b1ac5ed6546726c22343aebd
DESKTOP-CEJDPKV\t1003 <t100310414@ntut.org.tw>
2018-07-16 19:46:05 +0800

    fix(package): remove bug refactor(base): tab

--- src/app/reducers/base.js
+++ src/app/reducers/base.js
@@ -1,23 +1,23 @@
-import { 
+import {
 	CHANGE_LANGUAGE,
 	FETCH_LOCALE_SUCCESS,
-	FETCH_LOCALE_FAILED 
+	FETCH_LOCALE_FAILED
 } from '../actions/base';
 
 const INITIAL_STATE = {
-	lang:'zh',
+	lang: 'zh',
 	locale: null
 };
 
 export default function app(state = INITIAL_STATE, action) {
 	switch (action.type) {
-	case CHANGE_LANGUAGE:
-		return { ...state, lang:action.payload};
-	case FETCH_LOCALE_SUCCESS:
-		return { ...state, locale:action.payload};
-	case FETCH_LOCALE_FAILED:
-		return { ...state, locale:action.payload};
-	default:
-		return state;
+		case CHANGE_LANGUAGE:
+			return { ...state, lang: action.payload };
+		case FETCH_LOCALE_SUCCESS:
+			return { ...state, locale: action.payload };
+		case FETCH_LOCALE_FAILED:
+			return { ...state, locale: action.payload };
+		default:
+			return state;
 	}
 }
