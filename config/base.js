5eebd5d4ad92065cef47d49e305cf72cfa9ddd50
Hsun <t100310414@ntut.org.tw>
2019-10-30 22:36:47 +0800

    feat(all): add eslint

--- config/base.js
+++ config/base.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = function () {
+module.exports = function() {
 	return {
 		entry: {
-			'polyfill': 'babel-polyfill',
-			'vendor': [
+			polyfill: 'babel-polyfill',
+			vendor: [
 				'react',
 				'react-dom',
 				'redux',
@@ -13,9 +13,9 @@ module.exports = function () {
 				'react-router',
 				'react-intl',
 				'react-transition-group',
-				'classnames'
+				'classnames',
 			],
-			'main': './src/index.jsx'
+			main: './src/index.jsx',
 		},
 		devtool: 'source-map',
 		externals: 'Graph',
@@ -24,41 +24,46 @@ module.exports = function () {
 			extensions: ['.js', '.jsx', '.json'],
 			//entry裡import的路徑會從src node_modules裡開始查找
 			//使用path取得資料夾完整路徑，不然要自己手動copy __dirname並修改路徑
-			modules: [path.join(__dirname, '/../src'), 'node_modules']
+			modules: [path.join(__dirname, '/../src'), 'node_modules'],
 		},
 		module: {
-			rules: [{
-				enforce: 'pre',
-				test: /\.(js|jsx)$/,
-				//exclude: /node_modules/,
-				loader: 'eslint-loader',
-				options: {
-					cache: false,
-					failOnError: false,
-					formatter: require('eslint-friendly-formatter'),
-				}
-			}, {
-				test: /\.(js|jsx)$/,
-				//exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					//被轉換的結果將會被暫存起來。當Webpack再次編譯時，將會首先嘗試從暫存中讀取
-					cacheDirectory: true
-				}
-			}, {
-				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-				loader: 'url-loader',
-				options: {
-					limit: 8192,
-					name: 'images/[name].[ext]'
-				}
-			}, {
-				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-				loader: 'url-loader',
-				options: {
-					limit: 8192
-				}
-			}],
+			rules: [
+				{
+					enforce: 'pre',
+					test: /\.(js|jsx)$/,
+					//exclude: /node_modules/,
+					loader: 'eslint-loader',
+					options: {
+						cache: false,
+						failOnError: false,
+						formatter: require('eslint-formatter-pretty'),
+					},
+				},
+				{
+					test: /\.(js|jsx)$/,
+					//exclude: /node_modules/,
+					loader: 'babel-loader',
+					options: {
+						//被轉換的結果將會被暫存起來。當Webpack再次編譯時，將會首先嘗試從暫存中讀取
+						cacheDirectory: true,
+					},
+				},
+				{
+					test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+					loader: 'url-loader',
+					options: {
+						limit: 8192,
+						name: 'images/[name].[ext]',
+					},
+				},
+				{
+					test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+					loader: 'url-loader',
+					options: {
+						limit: 8192,
+					},
+				},
+			],
 		},
 		plugins: [
 			//自動產生首頁
@@ -70,8 +75,8 @@ module.exports = function () {
 				//script引用順序，按依賴關係
 				chunksSortMode: 'dependency',
 				template: 'src/index.html',
-				excludeChunks: 'manifest'
-			})
-		]
+				excludeChunks: 'manifest',
+			}),
+		],
 	};
-}
+};
