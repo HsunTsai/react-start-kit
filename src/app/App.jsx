98ea73fd47ddad11cd2e3c5bebfe9231391efb84
Hsun <t100310414@ntut.org.tw>
2019-11-02 13:39:42 +0800

    feat(App): add react-router-cache-route style(containers): add flex 1

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,75 +1,44 @@
-import React, { lazy, Suspense } from 'react';
-// import PropTypes from 'prop-types';
-// import { connect } from 'react-redux';
+import React from 'react';
 import {
 	BrowserRouter as Router,
 	Route,
 	Switch,
 	Redirect,
 } from 'react-router-dom';
-import 'antd/dist/antd.css';
+/* You can cache page when page cahnge by import CacheRoute & CacheSwitch */
+// import CacheRoute, { CacheSwitch } from 'react-router-cache-route';
 
 import services from './config/services';
 import Header from './components/Header';
 
-// LazyLoader
-const HomeComponent = lazy(() => import('./containers/Home'));
-const AboutComponent = lazy(() => import('./containers/About'));
-const TopicsComponent = lazy(() => import('./containers/Topics'));
-
-const LoadingMessage = () => 'Loading...';
+import HomeComponent from './containers/Home';
+import AboutComponent from './containers/About';
+import TopicsComponent from './containers/Topics';
 
 const App = () => {
 	const pages = [
-		{ path: '/home', name: 'Home', component: <HomeComponent /> },
-		{ path: '/about', name: 'About', component: <AboutComponent /> },
-		{ path: '/topic', name: 'Topic', component: <TopicsComponent /> },
+		{ path: '/home', name: 'Home', component: HomeComponent },
+		{ path: '/about', name: 'About', component: AboutComponent },
+		{ path: '/topic', name: 'Topic', component: TopicsComponent },
 	];
 	return (
 		<Router basename={services.getContextRoot}>
 			<div className="app">
 				<Header pages={pages} />
-				<Suspense fallback={<LoadingMessage />}>
-					<Switch>
-						{pages.map((page, index) => (
-							<Route
-								key={index.toString()}
-								exact={index === 0}
-								path={page.path}
-							>
-								{page.component}
-							</Route>
-						))}
-						<Redirect to={pages[0].path} />
-					</Switch>
-				</Suspense>
+				<Switch>
+					{pages.map((page, index) => (
+						<Route
+							key={index.toString()}
+							exact
+							path={page.path}
+							component={page.component}
+						/>
+					))}
+					<Redirect to={pages[0].path} />
+				</Switch>
 			</div>
 		</Router>
 	);
 };
 
-// App.defaultProps = {
-// 	language: null,
-// };
-
-// App.propTypes = {
-// 	language: PropTypes.objectOf(PropTypes.any),
-// };
-
-// const mapStateToProps = state => {
-// 	const { language } = state.app;
-// 	return { language };
-// };
-
-// export default connect(mapStateToProps)(App);
-
 export default App;
-
-// UNSAFE_componentWillReceiveProps(nextProps) {
-// 	const { language } = this.props;
-// 	if (language !== nextProps.language) {
-// 		setTimeout(() => {
-// 			this.forceUpdate();
-// 		}, 3000);
-// 	}
-// }
